ef594a4be2585790ee68fcc826bc03a305296f06
Zirpe-Dnyaneshwar <zirpepatil@gmail.com>
2025-09-29 13:44:41 +0530

    Connect Mysql Database from Contact From

--- Portfolio_app/views.py
+++ Portfolio_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import contact_data
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def home(request):
+    if request.method=='GET':
+        return render(request,'home.html')
+    else:
+        contact_data(
+            name=request.POST.get('Name'),
+            email=request.POST.get('Email'),
+            subject=request.POST.get('Subject'),
+            msg=request.POST.get('Message')
+        ).save()
+        return render(request,'home.html')
