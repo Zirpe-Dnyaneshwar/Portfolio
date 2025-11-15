032f6d81cda13190c422cbf5f171c76ffd329484
Zirpe-Dnyaneshwar <zirpepatil@gmail.com>
2025-11-15 15:32:42 +0530

    Commit

---
+++ Portfolio.js
@@ -0,0 +1,28 @@
+function ALL()
+{
+    document.getElementsByClassName('row')[0].style.display="block";
+    // document.getElementsByClassName('row')[1].style.display="block";
+    // document.getElementsByClassName('row')[2].style.display="block";
+    // document.getElementsByClassName('row')[3].style.display="block";
+}
+function APP()
+{
+    document.getElementsByClassName('row1')[1].style.display="block";
+    document.getElementsByClassName('row1')[0].style.display="none";
+    document.getElementsByClassName('row1')[2].style.display="none";
+    document.getElementsByClassName('row1')[3].style.display="none";
+}
+function CARD()
+{
+    document.getElementsByClassName('row2')[2].style.display="block";
+    document.getElementsByClassName('row2')[0].style.display="none";
+    document.getElementsByClassName('row2')[1].style.display="none";
+    document.getElementsByClassName('row2')[3].style.display="none";  
+}
+function SHOW()
+{
+    document.getElementsByClassName('row3')[3].style.display="block";
+    document.getElementsByClassName('row3')[0].style.display="none";
+    document.getElementsByClassName('row3')[1].style.display="none";
+    document.getElementsByClassName('row3')[2].style.display="none"; 
+}
